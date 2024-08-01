5365bafecaf5c27806f5255ab61eaaeccc9d0433
Mehmet Coşar <csrmehmett@gmail.com>
2024-08-01 16:50:21 +0300

    gorevler bitti

--- index.js
+++ index.js
@@ -50,8 +50,9 @@ function KareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function CemberinCevresi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinCevresi(yaricap) {
+let cevre = 2 * pi * yaricap;
+  return cevre
 }
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -64,8 +65,15 @@ function CemberinCevresi(/* kodlar buraya */) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(yaricap,pi) {
+ 
+ 
+  const alan = pi *  Math.pow(yaricap,2);
+  return alan
+  
+
+
+  
 }
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -98,28 +106,66 @@ let ucetambolunenler,
   tekraredensayilar;
 
 // 3a çözümü
+enkucuk = sayilar[0];
+  enbuyuk = sayilar[0];
+for (let i = 1 ; i < sayilar.length ; i++){
+  
+  if (sayilar[i]< enkucuk){
+    enkucuk = sayilar[i];
+  }
 
-/* kodlar buraya */
+ if (sayilar[i] > enbuyuk){
+    enbuyuk = sayilar[i];
+  }
+  
+}
+
 
 // 3b çözümü:
 
-/* kodlar buraya */
+ucetambolunenler = [];
+
+
+sayilar.forEach(sayi => {
+  if (sayi % 3 === 0) {
+    ucetambolunenler.push(sayi);
+  }
+});
+
 
 // 3c çözümü:
 
-/* kodlar buraya */
+ ucebolunenlerintoplami = ucetambolunenler.reduce((toplam, sayi) => toplam + sayi, 0);
+
 
 // 3d çözümü
 
-/* kodlar buraya */
+ besyuzdenkucuksayilar = sayilar.filter(sayi => sayi < 500);
 
 // 3e çözümü
 
-/* kodlar buraya */
+siralisayilar = besyuzdenkucuksayilar.slice().sort((a, b) => a - b);
+
 
 // 3f çözümü
 
-/* kodlar buraya */
+const tekrarEdilenSayilar = {};
+
+
+for (const sayi of sayilar) {
+  tekrarEdilenSayilar[sayi] = (tekrarEdilenSayilar[sayi] || 0) + 1;
+}
+
+
+ tekraredensayilar = [];
+
+
+for (const sayi in tekrarEdilenSayilar) {
+  const tekrarSayisi = tekrarEdilenSayilar[sayi];
+  if (tekrarSayisi > 1) {
+    tekraredensayilar.push(`${sayi} sayısı ${tekrarSayisi} kere tekrar edilmiştir`);
+  }
+}
 
 /*  Bu satırın aşağısındaki kodları lütfen değiştirmeyin  */
 
